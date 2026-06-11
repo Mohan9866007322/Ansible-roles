7d6ddb72f6abe97273f70f85d09270c07e957f2b
mohan6117vja@gmail.com <mohan6117vja@gmail.com>
2026-06-11 17:00:48 +0530

    roles

--- Roles-Ecom-Project/AMA.sh
+++ Roles-Ecom-Project/AMA.sh
@@ -3,8 +3,8 @@
 AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-01acce4059e4e17ee" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
-ZONE_ID="Z032558618100M4EJX8X4" # replace with your ZONE ID
-DOMAIN_NAME="daws84s.site" # replace with your domain
+ZONE_ID="Z05496083TGA6W64AEKNJ" # replace with your ZONE ID
+DOMAIN_NAME="marpukosam.xyz" # replace with your domain
 
 #for instance in ${INSTANCES[@]}
 for instance in $@

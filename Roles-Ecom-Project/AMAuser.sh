205ec15303f3e88e47ac8601b075f8ad5819f120
mohan6117vja@gmail.com <mohan6117vja@gmail.com>
2026-06-11 19:01:50 +0530

    roles

--- Roles-Ecom-Project/AMAuser.sh
+++ Roles-Ecom-Project/AMAuser.sh
@@ -1,38 +1,51 @@
 #!/bin/bash
 
-AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-01acce4059e4e17ee" # replace with your SG ID
+AMI_ID="ami-09e69ca1171857250"
+SG_ID="sg-0b68b6dbf6ae3c299" # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 ZONE_ID="Z05496083TGA6W64AEKNJ" # replace with your ZONE ID
 DOMAIN_NAME="marpukosam.xyz" # replace with your domain
 
-# Create a user-data script to set password and enable password authentication
-cat <<EOF > userdata.sh
+# 1. Create the User Data script locally (UPDATED)
+cat <<EOF > enable_ssh_password.sh
 #!/bin/bash
+# Set the password for ec2-user
 echo "ec2-user:DevOps321" | chpasswd
-sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-sed -i 's/^#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+
+# Enable Password Authentication in the main SSH config
+sed -i 's/^.*PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config
+
+# Forcefully enable it in the drop-in directory used by modern AMIs (Amazon Linux 2023, RHEL 9, etc.)
+if [ -d /etc/ssh/sshd_config.d ]; then
+    echo "PasswordAuthentication yes" > /etc/ssh/sshd_config.d/99-custom-auth.conf
+fi
+
+# Restart the SSH service to apply changes
 systemctl restart sshd
 EOF
 
 #for instance in ${INSTANCES[@]}
 for instance in $@
 do
-    # Note: Updated to use variables $AMI_ID and $SG_ID, and added --user-data
+    echo "Creating instance: $instance..."
+    
+    # 2. Pass the user data file during instance creation
     INSTANCE_ID=$(aws ec2 run-instances \
         --image-id $AMI_ID \
         --instance-type t3.micro \
         --security-group-ids $SG_ID \
-        --user-data file://userdata.sh \
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" \
+        --user-data file://enable_ssh_password.sh \
         --query "Instances[0].InstanceId" \
         --output text)
         
-    if [ $instance != "frontend" ]
+    if [ "$instance" != "frontend" ]
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
     else
+        # Wait until running to fetch the public IP reliably
+        aws ec2 wait instance-running --instance-ids $INSTANCE_ID
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
         RECORD_NAME="$DOMAIN_NAME"
     fi
@@ -43,7 +56,7 @@ do
     --hosted-zone-id $ZONE_ID \
     --change-batch '
     {
-        "Comment": "Creating or Updating a record set for '$instance' endpoint"
+        "Comment": "Creating or Updating a record set for '$instance'"
         ,"Changes": [{
         "Action"              : "UPSERT"
         ,"ResourceRecordSet"  : {
@@ -58,5 +71,5 @@ do
     }'
 done
 
-# Clean up the temporary user-data file
-rm -f userdata.sh
+# Clean up the temporary user data file
+rm -f enable_ssh_password.sh
